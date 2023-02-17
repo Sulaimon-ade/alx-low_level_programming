de167e9e0ce73915f5e0c87b7fe7e5790f204483
Sulaimon-ade <okunoyesouleiman@gmail.com>
2023-02-17 02:33:20 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry 
- * Return: Always 0
+ * main - entry
+ * return: always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
