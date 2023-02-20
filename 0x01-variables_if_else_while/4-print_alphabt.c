3abb10dead8b5ae87042560d18f1259fc5355452
Sulaimon-ade <okunoyesouleiman@gmail.com>
2023-02-20 03:22:56 -0800

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,19 @@
  * Return: always 0
  */
 int main(void)
+{
 	char c;
+
 	c = 'a';
+	{
 	while
-		(c <= 'z') {
-			if (( c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
-			c++;
-		}
+		(c <= 'z');
+	}
+	{
+	if ((c != 'q' && c != 'e') && c <= 'z')
+	putchar(c);
+	c++;
+	}
 	putchar('\n');
 	return (0);
+}
