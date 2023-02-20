ed2513562d51fdda07a545033dca345ec8de89b7
Sulaimon-ade <okunoyesouleiman@gmail.com>
2023-02-20 15:46:44 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; 1 < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
