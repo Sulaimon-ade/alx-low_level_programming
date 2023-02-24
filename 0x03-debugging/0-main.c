b7dca1d2fb34875925cc3ed6d53983ad2549906e
Sulaimon-ade <okunoyesouleiman@gmail.com>
2023-02-24 01:00:34 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,16 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: Always 0
+* Return: 0
 */
 
-void positive_or_negative(int i)
+
+int main(void)
+
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
