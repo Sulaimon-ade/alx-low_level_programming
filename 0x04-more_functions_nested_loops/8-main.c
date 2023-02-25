7842d47efb404bf63d05222ca292897d6a00e8a8
Sulaimon-ade <okunoyesouleiman@gmail.com>
2023-02-24 18:30:56 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+julien@ubuntu:~/0x04$ cat 8-main.c 
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
+}
+
