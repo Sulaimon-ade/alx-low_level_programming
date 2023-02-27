3722dce6a289f98f841b63eb8663476a1824d3df
Sulaimon-ade <okunoyesouleiman@gmail.com>
2023-02-27 02:37:47 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,40 +1,16 @@
 #include "main.h"
-
 /**
- * _atoi - Convert a string to an integer.
- * @s: The pointer to convert
- *
- * Return: A integer
+ * _strlen - return the length of a string
+ * Owned By Bwave
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
  */
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int a = 0;
 
-	while (s[c])
-	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-
-		if (isi == 1)
-		{
-			break;
-		}
-
-		c++;
-	}
-
-	ni *= min;
-	return (ni);
+	for (; *s++;)
+		a++;
+	return (a);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,1 @@
+cime back to see
