a6e212835a73fbbae643c74f7d642edde7ecda81
Sulaimon-ade <okunoyesouleiman@gmail.com>
2023-02-27 03:29:10 -0800

    pushing the putchar

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
-cime back to see
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
