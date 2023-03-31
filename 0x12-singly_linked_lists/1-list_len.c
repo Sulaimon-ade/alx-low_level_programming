b9324f4e3f3335f4a2a5b7e9e8b000711ca50eba
sulaimon-ade <okunoyesouleiman@gmail.com>
2023-03-31 13:37:05 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: Name of the list
+ * Return: The number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
